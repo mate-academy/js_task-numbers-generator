cdd97307c96bca6b30fa893b599e8ed6803b989b
ArtemMarkevych1 <aw.markevich@gmail.com>
2019-08-23 18:01:16 +0300

    Solution

--- src/createNumbersGenerator.js
+++ src/createNumbersGenerator.js
@@ -25,8 +25,11 @@
  *
  * @return {number}
  */
-function createNumbersGenerator(numbers, min, max) {
-  // write code here
+function createNumbersGenerator(numbers = [], min = -Infinity, max = Infinity) {
+  let index = 0;
+  const listOfNum = numbers.filter(elem => elem >= min && elem <= max);
+
+  return () => listOfNum[index++];
 }
 
 module.exports = createNumbersGenerator;
