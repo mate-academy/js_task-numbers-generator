26ec83268e62a731e8dc0bc65c375adf0802bef1
Volodsher <volodsher@gmail.com>
2019-08-24 13:55:35 +0300

    6 but 1

--- src/createNumbersGenerator.js
+++ src/createNumbersGenerator.js
@@ -25,8 +25,49 @@
  *
  * @return {number}
  */
+// let min = 2;
+// let max = 5;
+// let g = [1, 4, 5, 6, 4, 3, 5];
+// console.log(5 > 4 && 5 < 5)
+// g = g.filter(el => el > min && el < max);
+// console.log(g);
+
 function createNumbersGenerator(numbers, min, max) {
-  // write code here
+  let rawNumbers = numbers;
+  const newNumbers = [];
+  let a = 0;
+  if (rawNumbers === undefined) {
+    return undefined;
+  }
+  if (min !== undefined && max !== undefined) {
+    rawNumbers = rawNumbers.filter(element =>
+      element >= min && element <= max
+    );
+  };
+  for (let i = 0; i < 2; i++) {
+    newNumbers.push(rawNumbers[i]);
+  }
+
+  return function generator() {
+    if (numbers === undefined) {
+      return undefined;
+    }
+    const b = a;
+    a++;
+    return newNumbers[b];
+  };
 }
+
+// const z = createNumbersGenerator([1, 3, 0, 2, 5], 2, 4);
+// const q = createNumbersGenerator();
+// console.log(q());
+// console.log(q);
+
+// console.log(z());
+// console.log(z());
+// console.log(z());
+// console.log(z());
+// console.log(z());
+// console.log(z());
 
 module.exports = createNumbersGenerator;
