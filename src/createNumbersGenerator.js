b8c2c6b1ff2107b00ca264e112aaf1fd9274b851
cs-cognitome <gaya.scienza.v@gmail.com>
2019-11-25 20:07:19 +0200

    solution

--- src/createNumbersGenerator.js
+++ src/createNumbersGenerator.js
@@ -25,8 +25,13 @@
  *
  * @return {number}
  */
-function createNumbersGenerator(numbers, min, max) {
-  // write code here
+function createNumbersGenerator(numbers = [], min, max) {
+  const range = min && max ? numbers
+    .filter(number => number >= min && number <= max) : numbers;
+
+  return function() {
+    return range.shift();
+  };
 }
 
 module.exports = createNumbersGenerator;
