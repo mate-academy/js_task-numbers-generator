b16423f2764e24451fd3f8a5a154bea4c40c38f4
Misha Grinko <glukym@gmail.com>
2020-04-06 09:22:09 +0300

    Update createNumbersGenerator.js

--- src/createNumbersGenerator.js
+++ src/createNumbersGenerator.js
@@ -2,10 +2,11 @@
 
 /**
  * Write a function (factory) accepting `numbers` array and returning a function
- * (device). Each device call should return the next number from the array.
- * If `min` and `max` were provided the device should return only numbers which
- * are not less than `min` and not greater than `max`). If there is no more
- * matching numbers in the array return `undefined`.
+ * (device). Each device call should return the next number from the array. The
+ * factory function should also accept `min` and `max`. If `min` and `max` are
+ * provided the device should return only numbers which are not less than `min`
+ * and not greater than `max`. If there is no more matching numbers in the array
+ * return `undefined`.
  *
  * For example:
  *
