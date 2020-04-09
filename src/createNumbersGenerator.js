58316d85cf0679a52acaac142a5f6e7e37c5621f
Illia Oliynik <illiaoliynik@gmail.com>
2020-04-09 17:32:41 +0300

    solution

--- src/createNumbersGenerator.js
+++ src/createNumbersGenerator.js
@@ -32,7 +32,22 @@
  * @return {number}
  */
 function createNumbersGenerator(numbers, min, max) {
-  // write code here
+  let index = 0;
+
+  const fun = () => {
+    let num = numbers[index];
+
+    while (num < min || num > max) {
+      index++;
+      num = numbers[index];
+    }
+
+    index++;
+
+    return num;
+  };
+
+  return fun;
 }
 
 module.exports = createNumbersGenerator;
