0923d035838a82d36b0f90bf5db87d3d9b4fb634
VlaDinter <vladyslavdinter@gmail.com>
2020-06-05 19:26:00 +0300

    Solution

--- src/createNumbersGenerator.js
+++ src/createNumbersGenerator.js
@@ -1,36 +1,19 @@
 'use strict';
 
-/**
- * Write a function (factory) accepting `numbers` array and returning a function
- * (device). Each device call should return the next number from the array. The
- * factory function should also accept `min` and `max`. If `min` and `max` are
- * provided the device should return only numbers which are not less than `min`
- * and not greater than `max`. If there is no more matching numbers in the array
- * return `undefined`.
- *
- * For example:
- *
- * const generator1 = createNumbersGenerator([1, 3])
- * console.log(generator1()); // 1
- * console.log(generator1()); // 3
- * console.log(generator1()); // undefined
- *
- * const numbers = [1, 0, 3, 2, 5];
- * const generator2 = createNumbersGenerator(numbers, 1, 3)
- * console.log(generator2()); // 1
- * console.log(generator2()); // 3
- * console.log(generator2()); // 2
- * console.log(generator2()); // undefined
- *
- * const generator3 = createNumbersGenerator([])
- * console.log(generator3()); // undefined
- *
- * @param {number[]} numbers - array of numbers to return
- * @param {number} min - (optional)
- * @param {number} max - (optional)
- */
-function createNumbersGenerator(numbers, min, max) {
-  // write code here
+function createNumbersGenerator(
+  numbers,
+  min = Math.min(...numbers),
+  max = Math.max(...numbers)
+) {
+  return () => {
+    let number = numbers.shift();
+
+    while (number < min || number > max) {
+      number = numbers.shift();
+    }
+
+    return number;
+  };
 }
 
 module.exports = createNumbersGenerator;
